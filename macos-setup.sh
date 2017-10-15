f4ba8b4ad4e4cd4d4f1b2a6f7291383ee050192e
Sahil Sehwag <sehwagsahil002@gmail.com>
2017-10-15 11:10:16 +0530

    Homebrew/Cask Installation and Added GUI packages

--- macos-setup.sh
+++ macos-setup.sh
@@ -1,11 +1,19 @@
 #! /bin/sh
 
 #VARIABLES
+	DRIVE=~/Google\ Drive
+#HOMEBREW
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	brew tap caskroom/cask
 #SETUP
 	brew install git
 	brew install wget
 	brew install curl
-#APPLICATIONS
+#DEVELOPMENT
+	brew install python3
+	brew install node
+	brew install go
+#CLI
 	#UTILITIES
 		brew install fasd
 		brew install dos2unix
@@ -22,7 +30,17 @@
 		brew install fzf
 	#VIFM
 		brew install vifm
-#DEVELOPMENT
-	brew install python3
-	brew install node
-	brew install go
+#GUI
+	brew cask install iterm2
+	brew cask install alfred
+	brew cask install google-backup-and-sync
+	brew cask install visual-studio-code
+	brew cask install google-chrome
+	brew cask install spotify
+	brew cask install cleanmymac
+	brew cask install whatsapp
+	brew cask install bittorrent
+	brew cask install vlc
+	brew cask install shimo
+	#brew cask install macvim
+	#brew cask install foldingtext
